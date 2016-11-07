9e2bd9a463cbb36a52779ca480c1509f9c569691
Dmitry Latnikov <dlatnikov@griddynamics.com>
2016-11-07 16:15:08 +0300

    update for release 1.2.6 (#671)

--- chassis/core/src/main/java/com/griddynamics/jagger/invoker/Invoker.java
+++ chassis/core/src/main/java/com/griddynamics/jagger/invoker/Invoker.java
@@ -71,15 +71,15 @@ public interface Invoker<Q,R,E> extends Serializable {
 /// @li @ref Main_Listeners_General_group
 /// @li @ref Main_Providers_General_group
 /// @li @ref Main_Services_General_group
-/// @li @ref Main_Decision_Maker_General_group @e @htmlonly <font color="#009900">NEW ver 1.2.5</font>@endhtmlonly
+/// @li @ref Main_Decision_Maker_General_group
 ///
 /// @li @ref Main_Custom_Components_group
 ///
-/// @li @ref Main_Properties_group @e @htmlonly <font color="#009900">NEW ver 1.2.5</font>@endhtmlonly
+/// @li @ref Main_Properties_group
 ///
 /// @li @ref WebUiMain
 ///
-/// @li @ref MonitoringMain @e @htmlonly <font color="#009900">NEW ver 1.2.5</font>@endhtmlonly
+/// @li @ref MonitoringMain
 ///
 /// @n
 /// @n
